360aa50287da4d97cfc451a3f1aae6b0d513ff59
Majo-2306 <maria_castro24@unikino.edu.mx>
2024-11-12 08:02:40 -0700

    Agregar archivos de proyecto.

---
+++ ConversionesDeMoneda/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConversionesDeMoneda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                MostrarMenu();
+
+                switch (Console.ReadLine().ToString())
+                {
+                    case "1":
+                        Console.WriteLine("Pesos a Dolares");
+                        break;
+                    case "2":
+                        Console.WriteLine("Dolares a Pesos");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion Incorrecta");
+                        break;
+                }
+                Console.ReadLine();
+            }
+            //decimal conversion = PesosADolares(20.43m, 50000m);
+            //Console.WriteLine(conversion.ToString("N2"));
+        }
+
+        static void MostrarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Conversiones");
+            Console.WriteLine("1) Pesos a Dolares");
+            Console.WriteLine("2) Dolares a Pesos");
+            Console.WriteLine("");
+        }
+
+        static decimal PesosADolares(decimal tipoCambio, decimal pesos)
+        {
+            decimal dolares = 0.00m;
+            dolares = pesos / tipoCambio;
+            return dolares;
+        }
+    }
+}
